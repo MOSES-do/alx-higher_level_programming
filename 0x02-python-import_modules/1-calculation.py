4476d52512e3ba711b44cac2da6afd664f7e31fa
Esumei Moses <archer.me24@gmail.com>
2024-01-04 04:45:51 -0800

    task 0-2 update

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,12 +2,10 @@
 from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
-
 resultOfADD = add(a, b)
 resultofSUB = sub(a, b)
 resultofMUL = mul(a, b)
 resultofDIV = div(a, b)
-
 if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, resultOfADD))
     print("{} - {} = {}".format(a, b, resultofSUB))
