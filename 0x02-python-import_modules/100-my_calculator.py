ce88523f5cceb601fbb16fb08e6bc362bca2e6dc
Esumei Moses <archer.me24@gmail.com>
2024-01-04 12:17:06 -0800

    100-my_calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
+
+def calc():
+
+    if len(sys.argv) < 2:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    
+    arg1 = sys.argv[1]
+    ope = sys.argv[2]
+    arg2 = sys.argv[3]
+
+    if ope == "+":
+        result = add(int(arg1), int(arg2))
+        print(f"{arg1} + {arg2} = {result}")
+    elif ope == "*":
+        result = mul(int(arg1), int(arg2))
+        print(f"{arg1} * {arg2} = {result}")
+    elif ope == "-":
+        result = sub(int(arg1), int(arg2))
+        print(f"{arg1} - {arg2} = {result}")
+    elif ope == "/":
+        result = div(int(arg1), int(arg2))
+        print(f"{arg1} / {arg2} = {result}")
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    calc()
