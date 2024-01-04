8f76f58edbc5fa6f54d64e949debe5c1e02c35a2
Esumei Moses <archer.me24@gmail.com>
2024-01-04 04:39:06 -0800

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import add_0
 
-a = 1
-b = 2
-c = add_0.add(a, b)
 if __name__ == "__main__":
+    import add_0
+
+    a = 1
+    b = 2
+    c = add_0.add(a, b)
     print("{} + {} = {}".format(a, b, c))
