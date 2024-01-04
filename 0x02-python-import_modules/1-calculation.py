490e3902a8deb8c6d0cc7631cc64b246d0a85eb3
Esumei Moses <archer.me24@gmail.com>
2024-01-04 01:38:45 -0800

    1-calculation

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
+
+resultOfADD = add(a, b)
+resultofSUB = sub(a, b)
+resultofMUL = mul(a, b)
+resultofDIV = div(a, b)
+
+print("{} + {} = {}".format(a, b, resultOfADD))
+print("{} - {} = {}".format(a, b, resultofSUB))
+print("{} * {} = {}".format(a, b, resultofMUL))
+print("{} / {} = {}".format(a, b, resultofDIV))
