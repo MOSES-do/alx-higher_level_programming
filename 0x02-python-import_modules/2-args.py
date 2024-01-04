cf13899d420a634293fd8ec2aee0143205b71e5b
Esumei Moses <archer.me24@gmail.com>
2024-01-04 05:12:20 -0800

    task 0&2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 import sys
-def printCmdArgvs():
-    args = sys.argv[1:]
-    num_args = len(args)
-    if num_args == 0:
-        print(f"{num_args} arguments.")
-    elif num_args == 1:
-        print(f"{num_args} argument:")
-        for i, arg in enumerate(args, start=1):
-            print(f"{i}: {arg}")
-    else:
-        print(f"{num_args} arguments:")
-        for i, arg in enumerate(args, start=1):
-            print(f"{i}: {arg}")
+
 if __name__ == "__main__":
     printCmdArgvs()
+
+    def printCmdArgvs():
+        args = sys.argv[1:]
+        num_args = len(args)
+        if num_args == 0:
+            print(f"{num_args} arguments.")
+        elif num_args == 1:
+            print(f"{num_args} argument:")
+            for i, arg in enumerate(args, start=1):
+                print(f"{i}: {arg}")
+        else:
+            print(f"{num_args} arguments:")
+            for i, arg in enumerate(args, start=1):
+                print(f"{i}: {arg}")
