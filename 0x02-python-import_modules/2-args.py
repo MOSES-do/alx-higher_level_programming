7f5e0b88cb82f8513fae6f4b3fa96271d31b82b1
Esumei Moses <archer.me24@gmail.com>
2024-01-04 05:26:24 -0800

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
 
+
 def printCmdArgvs():
     args = sys.argv[1:]
     num_args = len(args)
@@ -14,5 +15,7 @@ def printCmdArgvs():
         print(f"{num_args} arguments:")
         for i, arg in enumerate(args, start=1):
             print(f"{i}: {arg}")
+
+
 if __name__ == "__main__":
     printCmdArgvs()
