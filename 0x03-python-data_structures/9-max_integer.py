2ba1f56f4c3c7609b520d8b82a7730db9ad0ef2b
Esumei Moses <archer.me24@gmail.com>
2024-01-09 23:51:15 -0800

    find the maximum number in a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    temp = 0
+    temp = my_list[0];
     listLen = len(my_list) - 1
-    lenMinus = listLen - len(my_list[2 : -1])
-    if len(my_list) == 0:
+    #lenMinus = listLen - len(my_list[2 : -1])
+    
+    if len(my_list) <= 0:
         return None
     else:
-        while lenMinus > 1:
-            j = 0
-            while j < listLen:
-                """print("{}, {}".format(j, j+1))
-                print("\n")
-                print("{}-j, {}-j1".format(my_list[j], my_list[j+1]))
-                print("\n")"""
-                if my_list[j] < my_list[j+1]:
+        for i in range(listLen):
+            if my_list[i] > temp:
+                temp = my_list[i]
+    return temp
+
+"""while lenMinus > 1:
+        j = 0
+        while j < listLen:
+            if my_list[j] > my_list[j+1]:
+                temp = my_list[j]
+                my_list[j+1] = temp
+                else:
                     temp = my_list[j+1]
                     my_list[j] = temp
-                    """print("{:d}--tempup".format(temp))
-                    print("\n")"""
-                else:
-                    temp = my_list[j]
-                    my_list[j+1] = temp
-                    # print("{:d}--tempdown".format(temp))
                 j += 1
             lenMinus = -1
-        return temp
+        return temp"""
+
