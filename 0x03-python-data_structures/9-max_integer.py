3bb0b6e194dcbc29b652ab481dea40fdd88670c6
Esumei Moses <archer.me24@gmail.com>
2024-01-09 23:54:50 -0800

    print max number

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
+
 def max_integer(my_list=[]):
-    temp = my_list[0];
-    listLen = len(my_list) - 1
-    #lenMinus = listLen - len(my_list[2 : -1])
-    
+    temp = my_list[0]
+    listLen = len(my_list)
+
     if len(my_list) <= 0:
         return None
     else:
@@ -12,17 +12,3 @@ def max_integer(my_list=[]):
             if my_list[i] > temp:
                 temp = my_list[i]
     return temp
-
-"""while lenMinus > 1:
-        j = 0
-        while j < listLen:
-            if my_list[j] > my_list[j+1]:
-                temp = my_list[j]
-                my_list[j+1] = temp
-                else:
-                    temp = my_list[j+1]
-                    my_list[j] = temp
-                j += 1
-            lenMinus = -1
-        return temp"""
-
