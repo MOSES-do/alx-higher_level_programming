bb64eec65055ead6ebe5247356a7676d58bcf80a
Esumei Moses <archer.me24@gmail.com>
2024-01-09 17:42:35 -0800

    print first character of a string

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    senlen = len(sentence)
+
+    if senlen == 0:
+        return 0, None
+    elif senlen > 0:
+        return len(sentence), sentence[0] 
