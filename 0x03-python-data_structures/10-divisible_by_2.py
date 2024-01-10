9f2fce12422c82e0df59a1a1c7ade7874beb008e
Esumei Moses <archer.me24@gmail.com>
2024-01-09 19:56:03 -0800

    multiples of 2 ni al list

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    new_list = []
+    listlength = len(my_list)
+  
+    for i in range(listlength):
+        if my_list[i] % 2 == 0:
+            new_list.append(True)
+        else:
+            new_list.append(False)
+    return new_list
+        
