8b93424762c66756727ff76de05eb8045fce729e
Esumei Moses <archer.me24@gmail.com>
2024-01-09 19:12:42 -0800

    find the max number of a given list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    temp = 0
+    listLen = len(my_list) - 1
+    if len(my_list) == 0:
+        return None
+    else:
+        while listLen > 1:
+            j = 0
+            while j < listLen:
+                """print("{}, {}".format(j, j+1))
+                print("\n")
+                print("{}-j, {}-j1".format(my_list[j], my_list[j+1]))
+                print("\n")"""
+                if my_list[j] < my_list[j+1]:
+                    temp = my_list[j+1]
+                    my_list[j] = temp
+                    """print("{:d}--tempup".format(temp))
+                    print("\n")"""
+                else:
+                    temp = my_list[j]
+                    my_list[j+1] = temp
+                    # print("{:d}--tempdown".format(temp))
+                j += 1
