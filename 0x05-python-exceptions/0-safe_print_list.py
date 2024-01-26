fe45b87a401443f8785610832a302e9e396b8246
Esumei Moses <archer.me24@gmail.com>
2024-01-25 19:03:14 -0800

    task 0 print elements of a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,13 +8,14 @@ try:
         for i in my_list:
             number += 1
         if (x <= number):
-             for my_list in range(1, x+1):
+            for my_list in range(1, x+1):
                 count += 1
-                print("{}".format(my_list), end="\n")
+                print("{}".format(my_list), end="")
+        print('\n')
         if (x > number):
             for my_list in range(1, number+1):
-                print("{}".format(my_list), end="\n")
+                print("{}".format(my_list), end="")
             count = number
         return count
 except ValueError:
-        print(f"OOps, No error is can be caught", e)
+    print(f"OOps, No error is can be caught", e)
