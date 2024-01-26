5cb9f360ee39e83356f3e774e488ae3671204870
Esumei Moses <archer.me24@gmail.com>
2024-01-25 18:05:51 -0800

    task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+
+try:
+    def safe_print_list(my_list=[], x=0):
+        number = 0
+        if x > 5:
+            x = 5
+        for my_list in range(1, x+1):
+            number += 1
+            print("{}".format(my_list), end="\n")
+        return number
+except ValueError:
+    print(f"OOps, No error is can be caught", e)
