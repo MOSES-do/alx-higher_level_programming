645024aff2cff03b7b89fc864cbacd481636c474
Esumei Moses <archer.me24@gmail.com>
2024-01-29 20:20:35 -0800

    Instance method that returns an “informal” and nicely printable string representation of an instance

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+class Rectangle:
+    """Defines a Rectangle class"""
+    def __init__(self, width=0, height=0):
+        self.__height = height
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if (value < 0):
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif (value < 0):
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def area(self):
+        """calculates the area of a rectangle"""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """returns the perimeter of a rectangle"""
+        if (self.__width or self.__height == 0):
+            perimeter = 0
+        perimeter = (self.__width + self.__height) * 2
+        return perimeter
+
+    def __str__(self):
+        hashtag = ""
+        """Print the sqaure with the # character"""
+        if self.__width or self.__height == 0:
+            hashtag = ""
+        for i in range(0, self.__height):
+            for j in range(self.__width):
+                hashtag += "#"
+            hashtag += "\n"
+        return hashtag
+
+    """def __repr__(self):
+        #returns official string represenatio of rectangle
+        modl = type(self).__module__
+        klas = type(self).__name__
+        memad = hex(id(self))
+        return f"<{modl}.{klas} object at {memad}>"""
