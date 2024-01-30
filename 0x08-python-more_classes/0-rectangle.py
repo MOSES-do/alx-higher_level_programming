3bb1f823af1ce9aa27710b71d912c85e7d2256c7
Esumei Moses <archer.me24@gmail.com>
2024-01-30 02:14:54 -0800

    task 0

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -17,7 +17,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif (value < 0):
             raise ValueError("height must be >= 0")
