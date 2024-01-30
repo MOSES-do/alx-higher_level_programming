ff8e351d9ff6219cc71cc13603ecd91303136c6d
Esumei Moses <archer.me24@gmail.com>
2024-01-29 19:35:20 -0800

    Added doc strings and setter assignment value

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -29,3 +29,14 @@ class Rectangle:
         elif (value < 0):
             raise ValueError("width must be >= 0")
         self.__width = value
+
+    def area(self):
+        """calculates the area of a rectangle"""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """returns the perimeter of a rectangle"""
+        if (self.__width or self.__height == 0):
+            perimeter = 0
+        perimeter = (self.__width + self.__height) * 2
+        return perimeter
