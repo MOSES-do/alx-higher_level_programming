569169ce0de02218b924ccd159711f0249b7da30
Esumei Moses <archer.me24@gmail.com>
2024-01-30 02:24:02 -0800

    task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,11 +13,13 @@ class Rectangle:
 
     @property
     def height(self):
+        """function to return height value"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        if type(value) is not int:
+        """function to assign/change value of private variable"""
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif (value < 0):
             raise ValueError("height must be >= 0")
@@ -26,13 +28,26 @@ class Rectangle:
 
     @property
     def width(self):
+        """function to return width value"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """function to assign/change value of private variable"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         elif (value < 0):
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
+
+    def area(self):
+        """calculates the area of a rectangle"""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """returns the perimeter of a rectangle"""
+        if (self.__width == 0 or self.__height == 0):
+            perimeter = 0
+        perimeter = (self.__width + self.__height) * 2
+        return perimeter
