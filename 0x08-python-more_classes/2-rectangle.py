480a927b426999644295868be912a9c23f534d15
Esumei Moses <archer.me24@gmail.com>
2024-01-31 01:23:37 -0800

    tasks update

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -8,8 +8,8 @@ Definition of a Rectangle class
 class Rectangle:
     """Class definition of Rectangle instances"""
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def height(self):
