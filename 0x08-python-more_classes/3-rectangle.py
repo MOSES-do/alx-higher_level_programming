dab3c7f1e9f8d4046a3e6db88ac363c0ffab4529
Esumei Moses <archer.me24@gmail.com>
2024-01-31 02:08:20 -0800

    3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -49,7 +49,7 @@ class Rectangle:
     def __str__(self):
         hashtag = ""
         """Print the sqaure with the # character"""
-        if self.__width or self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             return ("")
         for i in range(self.__height):
             for j in range(self.__width):
