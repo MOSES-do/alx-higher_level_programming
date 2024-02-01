d45437b08a1b631eb8f52ffc0e0a9b548c11c30e
Esumei Moses <archer.me24@gmail.com>
2024-02-01 06:20:38 -0800

    task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    if type(a) not in [int]:
+        int_a = int(a)
+        a = int(a)
+
+    if type(b) not in [int]:
+       int_b = int(b)
+       b = int(b)
+
+    return (a + b)
