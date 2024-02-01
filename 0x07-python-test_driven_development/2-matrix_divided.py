200262d12afbd1706369c6a3dc941bf35aa3bfb3
Esumei Moses <archer.me24@gmail.com>
2024-02-01 14:30:07 -0800

    2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -11,24 +11,26 @@ def matrix_divided(matrix, div):
     """
     Divide elements of a list using a divisor
     """
+    str = "matrix must be a matrix (list of lists) of integers/floats)"
+    if (matrix == []):
+        raise TypeError(str)
 
-    str = "Each row of the matrix must have the same size"
     rowlen = []
     for i in range(len(matrix)):
         count = 0
         for j in range(len(matrix[i])):
             count += 1
-            if (not isinstance(matrix[i], list) or (not isinstance(matrix, list)) or matrix == []):
-                raise TypeError("matrix must be a matrix \
-                (list of lists) of integers/floats")
+            if not isinstance(matrix[i], list):
+                raise TypeError(str)
+            if not isinstance(matrix, list):
+                raise TypeError(str)
             if not isinstance(matrix[i][j], (int, float)):
-                raise TypeError("matrix must be a matrix \
-                (list of lists) of integers/floats")
+                raise TypeError(str)
         rowlen.append(count)
 
     for i in range(len(rowlen) - 1):
         if rowlen[i] is not rowlen[i+1]:
-            raise TypeError(str)
+            raise TypeError("Each row of the matrix must have the same size")
 
     if type(div) not in [int, float]:
         raise TypeError("div must be a number")
