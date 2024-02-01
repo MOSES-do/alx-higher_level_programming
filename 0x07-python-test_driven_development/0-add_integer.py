e7bd5a0c11e98fe2ef7ab89be6939d3ae2cd222a
Esumei Moses <archer.me24@gmail.com>
2024-02-01 06:42:57 -0800

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+"""
+
+This module provides a function that adds to integers
+
+"""
+
 
 def add_integer(a, b=98):
+
+    """
+	function add two integers
+	"""
+
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
