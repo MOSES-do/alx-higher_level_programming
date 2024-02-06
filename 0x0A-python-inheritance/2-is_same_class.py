232a075a84fba9ea84b1068a5bbbc4579b25bab8
Esumei Moses <archer.me24@gmail.com>
2024-02-06 00:44:01 -0800

    2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
+"""Function that checksif an object is of same class"""
+
 def is_same_class(obj, a_class):
+    """Function that checksif an object is of same class"""
     if (isinstance(obj, a_class)):
         return True
     else:
