fb9f66f01df07b76023a9204d9130ca0607ec3b2
Esumei Moses <archer.me24@gmail.com>
2024-02-06 01:01:54 -0800

    instance

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
+"""check if object is instance of a class"""
+
+
 def is_kind_of_class(obj, a_class):
+
+    """check if object is an instance of the class"""
+
     if (isinstance(obj, a_class)):
         return True
     else:
