dc16c2b44cf08cce7abf055b111ea7903c44cdf4
Esumei Moses <archer.me24@gmail.com>
2024-02-05 17:06:07 -0800

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,5 +6,5 @@
 class MyList(list):
     """Prints list a sorted list in ascending order"""
     def print_sorted(self):
-    """Prints list a sorted list in ascending order"""
+        """Prints list a sorted list in ascending order"""
         print(sorted(self))
