b76ed45ee964aec6a73afaa73127f89273b5aea3
Esumei Moses <archer.me24@gmail.com>
2024-02-06 00:59:52 -0800

    check instance of a class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -2,9 +2,10 @@
 
 """Function that checksif an object is of same class"""
 
+
 def is_same_class(obj, a_class):
     """Function that checksif an object is of same class"""
-    if (isinstance(obj, a_class)):
+    if type(obj) is a_class:
         return True
     else:
         return False
