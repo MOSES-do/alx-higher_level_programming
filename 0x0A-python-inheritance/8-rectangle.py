63365608a20d217478d3e33aa065156b94ec4a15
Esumei Moses <archer.me24@gmail.com>
2024-02-06 02:29:47 -0800

    8-rectangle.py

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+"""A geometry class"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+""" A Rectangle class"""
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle """
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+
+    def integer_validator(self, name, value):
+        super().integer_validator()
