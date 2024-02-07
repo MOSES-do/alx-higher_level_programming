15e208299feb559bc3f8692ce17959353ec04c29
Esumei Moses <archer.me24@gmail.com>
2024-02-06 17:21:04 -0800

    read from a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Function that reads a text file"""
+
+
+def read_file(filename=""):
+    """Read bytes of a file"""
+    with open(filename, encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data)
