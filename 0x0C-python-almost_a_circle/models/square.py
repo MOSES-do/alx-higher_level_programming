55d5034f0ee3724f41fca590b454b7bfb9e3ae78
Esumei Moses <archer.me24@gmaile.com>
2024-02-17 04:35:46 -0800

    args and **kwargs Square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,6 +9,11 @@ class Square(Rectangle):
         """constructor using super()"""
         super().__init__(size, size, x, y, id)
 
+    def __str__(self):
+        """The overloading __str__ method should return [Square]..."""
+
+        return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}")
+ 
     @property
     def size(self):
         """Getter"""
@@ -18,3 +23,35 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        
+        if args and len(args) != 0:
+            index = 0
+            for arg in args:
+                if index == 0:
+                    if (arg is None):
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif index == 1:
+                    self.size = arg
+                elif index == 2:
+                    self.x = arg
+                elif index == 3:
+                    self.y = arg
+                index += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    if value is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = value
+                elif key == "size":
+                    self.size = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
