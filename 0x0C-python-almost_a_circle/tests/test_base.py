a3ed0266401b7eb89f76844ac4e9b61b2b43f6e0
Esumei Moses <archer.me24@gmaile.com>
2024-02-17 05:47:48 -0800

    tests

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -1,7 +1,26 @@
 #!/usr/bin/python3
-""""""
+"""module to test string methods."""
 
-class TestBase(unittest.Test):
-    """"""
+import unittest
 
-    def test_num_arg(self):
+class TestStringMethods(unittest.TestCase):
+    """simple unittests for strings methods."""
+
+    def test_upper(self):
+        """checks for an expected result."""
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        """verifies a specified condition."""
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        """verify that a specific exception gets raised."""
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == "__main__":
+    unittest.main()
