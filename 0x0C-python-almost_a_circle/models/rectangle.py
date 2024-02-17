7505bda94fcdb44a4ed14a07a9fa1230716236c5
Esumei Moses <archer.me24@gmaile.com>
2024-02-16 22:48:00 -0800

    validate instance attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,7 +8,7 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.width = width
-        self. height = height
+        self.height = height
         self.x = x
         self.y = y
 
@@ -34,16 +34,32 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        if (not isinstance(value, int)):
+            raise TypeError("width  must be an integer")
+        elif (value < 0 or value == 0):
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @height.setter
     def height(self, value):
+        if (not isinstance(value, int)):
+            raise TypeError("height  must be an integer")
+        elif (value < 0 or value == 0):
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @x.setter
     def x(self, value):
+        if (not isinstance(value, int)):
+            raise TypeError("x must be an integer")
+        elif (value < 0):
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @y.setter
     def y(self, value):
+        if (not isinstance(value, int)):
+            raise TypeError("y must be an integer")
+        elif (value < 0):
+            raise ValueError("y must be >= 0")
         self.__y = value
