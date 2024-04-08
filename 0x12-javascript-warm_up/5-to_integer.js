9dd85e520cc5a762320dec02cef8654805ec4c97
Esumei Moses <archer.me24@gmail.com>
2024-04-08 11:11:10 -0700

    Task 5,6,7

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const { argv } = require('process')
-let num = +(argv[2])
+const { argv } = require('process');
+let num = +(argv[2]);
 
 if (isNaN(num)) {
-	console.log('Not a number')
+	console.log('Not a number');
 }
 else {
-	console.log(`My number: ${num}`)
+	console.log(`My number: ${num}`);
 }
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
 const progLanguages = ['C is fun', 'Python is cool', 'Javascript is amazing']
-//progLanguages.forEach((val) => console.log(`${val}`))
-/*for ( val of progLanguages) {
+
+/*progLanguages.forEach((val) => console.log(`${val}`))
+for ( val of progLanguages) {
 	console.log(val)
-}*/
-for (let num=0; num < progLanguages.length; num++) {
-	console.log(progLanguages[num])
+}
+*/
+
+for (let num = 0; num < progLanguages.length; num++) {
+	console.log(progLanguages[num]);
 }
