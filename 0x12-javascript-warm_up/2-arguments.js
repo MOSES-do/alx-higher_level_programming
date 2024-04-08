388721ab9fe1c909d2a1db06fdf2d3f8b5c525e3
Esumei Moses <archer.me24@gmail.com>
2024-04-08 11:48:52 -0700

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-const { argv } = require('node:process')
+const process = require('node:process')
+const args = process.argv
 
 if (argv[2] === undefined) {
 	console.log('No argument');
