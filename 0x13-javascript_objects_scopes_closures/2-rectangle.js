ad6e9e9a9a0e495d01e72c789643335b5f3d7db2
Esumei Moses <archer.me24@gmail.com>
2024-04-09 14:41:27 -0700

    Task 3 and 2 return update

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,9 +2,8 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w === 0 || w < 0 || h === 0 
-      || h < 0 || !h || !w) {
-      return 0; 
+    if (w <= 0 || h <= 0 || !h || !w) {
+      return; 
     } else {
       this.width = w;
       this.height = h;
--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,8 +2,7 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w === 0 || w < 0 || h === 0 
-      || h < 0 || !h || !w) {
+    if (w <= 0 || h <= 0 || !h || !w) {
       return 0; 
     } else {
       this.width = w;
