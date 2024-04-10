a94f3bb01b1a5da806a830a9c36f9d51c130ed0b
Esumei Moses <archer.me24@gmail.com>
2024-04-10 13:47:00 -0700

    9-logme.js

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -2,12 +2,12 @@
 
 let counter = 0;
 function logMe (item) {
-  return function () {
-    let curCounter = counter++;
+  return (function () {
+    const curCounter = counter++;
     console.log(`${curCounter}: ${item}`);
-  }("");
+  })('');
 }
 
 module.exports = {
   logMe
-}
+};
