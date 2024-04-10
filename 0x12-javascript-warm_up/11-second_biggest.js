6760503f3081da8c1f4971f4e7cce8dc96496c6b
Esumei Moses <archer.me24@gmail.com>
2024-04-10 14:17:41 -0700

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,20 +1,22 @@
 #!/usr/bin/node
 
-const { argv } = require('process')
+const { argv } = require('process');
 
 if (argv.length < 3) {
   console.log('0');
 }
-let arr = []
+const arr = [];
 
 for (let i = 2; i < argv.length; i++) {
   let num = +(argv[i]);
   arr.push(num);
 }
 
-/*for (val of arr) {
-	console.log(val)
-}*/
+/*
+for (val of arr) {
+  console.log(val)
+}
+*/
 
 if (arr.length === 1) console.log('0');
 
@@ -22,8 +24,8 @@ for (let i = 0; i < arr.length; i++) {
   for (let j = i+1; j < arr.length; j++) {
     let temp = arr[i];
     if (arr[i] > arr[j]) {
-      arr[i] = arr[j]
-      arr[j] = temp
+      arr[i] = arr[j];
+      arr[j] = temp;
     }
   }
 }
