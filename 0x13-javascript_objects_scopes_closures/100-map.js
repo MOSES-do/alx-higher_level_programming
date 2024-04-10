9188247d32280ffc520516edb6c6cfba2efd185b
Esumei Moses <archer.me24@gmail.com>
2024-04-09 17:44:56 -0700

    100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,12 +2,16 @@
 
 const list = require('./100-data').list;
 
-const newList = [];
+/*const newList = [];
 
 for (let i = 0; i < list.length; i++) {
   multiplier = i * list[i];
   newList.push(multiplier);
 }
+*/
 
+const newList = list.map((ele, idx) => {
+  return ele * idx;
+})
 console.log(list);
 console.log(newList);
