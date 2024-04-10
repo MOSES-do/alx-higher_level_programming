720ccfb639e1b0ade2600d11eb812cba466294fd
Esumei Moses <archer.me24@gmail.com>
2024-04-10 13:18:36 -0700

    3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,14 +2,14 @@
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0 || !h || !w) {
-      return 0; 
+      return 0;
     } else {
       this.width = w;
       this.height = h;
     }
   }
 
-  print() {
+  print () {
     for (let i = 0; i < this.height; i++) {
       let str = '';
       for (let j = 0; j < this.width; j++) {
