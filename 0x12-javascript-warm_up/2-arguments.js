1408652678c4758e1a0ebe7e3295769cc677f750
Esumei Moses <archer.me24@gmail.com>
2024-04-10 12:55:02 -0700

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,9 +3,9 @@ const process = require('node:process');
 const argv = process.argv;
 
 if (argv[2] === undefined) {
-	console.log('No argument');
+  console.log('No argument');
 } else if (argv.length === 3) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
