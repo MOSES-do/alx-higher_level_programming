828832cc9c1211c5f3a0fa7bad19835536ed0a12
Esumei Moses <archer.me24@gmail.com>
2024-04-10 13:27:44 -0700

    7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, searchElement) {
-    let count = 0;
+  let count = 0;
 
-    for (let i = 0; i < list.length; i++) {
-      let item = list[i];
+  for (let i = 0; i < list.length; i++) {
+    let item = list[i];
 
-      if (item === searchElement) {
-        count += 1;
-      }
+    if (item === searchElement) {
+      count += 1;
     }
-    return count;
+  }
+  return count;
 }
