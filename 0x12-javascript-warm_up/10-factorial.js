b7b8a785f69e4dfe7608b9f82152bd7e38942600
Esumei Moses <archer.me24@gmail.com>
2024-04-10 14:10:08 -0700

    10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,7 +2,7 @@
 
 const { argv } = require('process');
 
-let fac = Number(argv[2]);
+constt fac = Number(argv[2]);
 
 if (isNaN(fac)) {
   console.log('1');
@@ -11,7 +11,7 @@ if (isNaN(fac)) {
 
 function factorial (number){
   if (number <= 1) {
-    return 1
+    return 1;
   }
   return number * factorial(number - 1);
 }
