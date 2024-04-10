29a95a11cba5e0e083492ffbdb3e97d12a69b3d8
Esumei Moses <archer.me24@gmail.com>
2024-04-10 13:10:27 -0700

    semistandard update

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,9 +4,11 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
+
 myObject.incr = function() {
-  return myObject.value++
+  return myObject.value++;
 }
+
 myObject.incr();
 console.log(myObject);
 myObject.incr();
