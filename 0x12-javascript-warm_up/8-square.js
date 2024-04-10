776ce4a0a39acd37c9f5b00704289bef79dfb5ce
Esumei Moses <archer.me24@gmail.com>
2024-04-10 14:04:55 -0700

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,8 +5,8 @@ const num = +(argv[2]);
 if (isNaN(num)) console.log('Missing size');
 for (let i = 0; i < num; i++) {
   let xed = '';
-	for (let j = 0; j < num; j++) {
+  for (let j = 0; j < num; j++) {
     xed += 'X';
-	}
+  }
   console.log(xed);
 }
