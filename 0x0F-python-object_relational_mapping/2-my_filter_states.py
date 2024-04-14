8c7d11056b206fb146c2576f7482acb740f704ba
Esumei Moses <archer.me24@gmail.com>
2024-04-14 11:11:06 -0700

    task 1,2,3

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ import MySQLdb
 if len(sys.argv) < 4:
     sys.exit(1)
 
-user_input = sys.argv[4]
+ui = sys.argv[4]
 
 HOST = "localhost"
 user = "root"
@@ -21,7 +21,7 @@ def main():
 
     db = MySQLdb.connect(host=HOST, user=user, passwd="", db=d_base)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE '%{}%'".format(user_input))
+    cur.execute("SELECT * FROM states WHERE name LIKE '%{}%'".format(ui))
 
     result = cur.fetchone()
     print(result)
