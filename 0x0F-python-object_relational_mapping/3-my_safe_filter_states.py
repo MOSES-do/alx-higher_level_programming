4dc2378e52f5bce400d70dbb2be8b38e9b31839a
Esumei Moses <archer.me24@gmail.com>
2024-04-14 11:55:06 -0700

    Task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -13,17 +13,17 @@ user = "root"
 d_base = "hbtn_0e_0_usa"
 
 
-
 def main():
     """ Executing mysql query to get states  4&5 from database table """
 
     db = MySQLdb.connect(host=HOST, user=user, passwd="", db=d_base)
     cur = db.cursor()
-    query = f"SELECT * FROM states WHERE name = %s LIMIT 1"
+    query = f"SELECT * FROM states WHERE name = %s ORDER BY states.id \
+    ASC LIMIT 1"
     cur.execute(query, (user_input,))
 
     result = cur.fetchall()
-    
+
     for state in result:
         print(state)
     cur.close()
