ac01544c455d82b581d050edaf21c271ae46927d
Esumei Moses <archer.me24@gmail.com>
2024-04-15 03:15:52 -0700

    task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+""" This python file prints the name of states in hbtn_0e_0_usa database """
+
+
+import MySQLdb
+
+
+HOST = "localhost"
+user = "root"
+d_base = "hbtn_0e_4_usa"
+
+
+def main():
+    """ Executing mysql query to get states  4&5 from database table """
+
+    db = MySQLdb.connect(host=HOST, user=user, passwd="", db=d_base)
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities \
+                JOIN states ON cities.state_id = states.id")
+
+    result = cur.fetchall()
+
+    for states in result:
+        print(states)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
