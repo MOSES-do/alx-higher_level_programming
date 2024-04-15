6f4dbf38b0c50bebdd25da3cab6280a35e37c284
Esumei Moses <archer.me24@gmail.com>
2024-04-15 08:17:40 -0700

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -23,15 +23,15 @@ def main():
                     db=sys.argv[3]
                 )
     cur = db.cursor()
-    """query = "SELECT * FROM states WHERE name LIKE '{placeholder}' \
+    query = "SELECT * FROM states WHERE BINARY states.name LIKE '{placeholder}' \
     ORDER BY states.id"
     search_term = ui
     sql_query = query.format(placeholder=search_term)
-    cur.execute(sql_query)"""
-    query = "SELECT * FROM states WHERE '{field}' = %s \
+    cur.execute(sql_query)
+    """query = "SELECT * FROM states WHERE '{field}' = %s \
         ORDER BY states.id ASC"
-    sql_query = query.format(field=ui)
-    cur.execute(sql_query, (ui,))
+    sql_query = query.format(field=name)
+    cur.execute(sql_query, (ui,))"""
 
     result = cur.fetchall()
     for state in result:
