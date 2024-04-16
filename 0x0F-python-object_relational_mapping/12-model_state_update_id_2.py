aec5dfe94e124dc83449a263f59e1024b9ae3052
Esumei Moses <archer.me24@gmail.com>
2024-04-15 17:08:59 -0700

    12-model_state_update_id_2.py

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     session = Session()
 
     state = session.query(State) \
-        .filter(State.id == 2) \
+        .where(State.id == 2) \
         .update({"name": "Mexico"})
     session.commit()
     session.close()
