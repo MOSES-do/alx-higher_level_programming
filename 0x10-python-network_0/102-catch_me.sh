1ea9922d343ae325ee94d78dd170ea07db9710f8
Esumei Moses <archer.me24@gmail.com>
2024-04-26 08:05:32 -0700

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!
-curl -s "$1"
+curl -sIL GET http://0.0.0.0:5000/catch_me | grep -q "HTTP/1.1 405 METHOD NOT ALLOWED" && echo "You got me!"
