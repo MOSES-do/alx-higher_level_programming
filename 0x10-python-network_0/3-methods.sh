982a6896f942d4cdbabb857c9a8bc0f2e1a8a72d
Esumei Moses <archer.me24@gmail.com>
2024-04-26 05:29:44 -0700

    3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL and displays all HTTP methods the server will accept
-curl -sI -X OPTIONS "$1" | grep "Allow" | awk '{print $2 " " $3 " " $4}'
+curl -sI -X OPTIONS "$1" | grep "Allow" | sed 's/Allow: //'
