51150f7a1a04e3121777280a3e7b57b981dbc183
Esumei Moses <archer.me24@gmail.com>
2024-04-26 01:32:29 -0700

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script to send a request url ad display the size of the body response
-echo $(curl -s -o /dev/null -w "%{size_download}" "$url")
+curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}'
