d9f3b45e2fe25662b0e97d26833de090a999fb5a
Esumei Moses <archer.me24@gmail.com>
2024-04-25 19:50:07 -0700

    Python networking

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,15 +1,3 @@
 #!/usr/bin/env bash
 # Script to send a request url ad display the size of the body response
-
-# Check if a URL is provided as an argument
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-url="$1"
-
-response_bodysize=$(curl -s -o /dev/null -w "%{size_download}" "$url")
-
-# Display the size of the response body
-echo "$response_bodysize"
+echo $(curl -s -o /dev/null -w "%{size_download}" "$url")
