57c248aaedf08ba76a7a75fb990d09efea620ee6
Esumei Moses <archer.me24@gmail.com>
2024-04-26 07:49:30 -0700

    5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 # POST request to server
-curl -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
