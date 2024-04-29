bdd21d67702ed24935bc3f19beec12b9b5b070fa
Esumei Moses <archer.me24@gmail.com>
2024-04-29 15:52:19 -0700

    fetch using urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,10 +3,11 @@
 import urllib.request
 
 
-URI = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-with urllib.request.urlopen(URI) as response:
-  html = response.read()
-  print("Body response:")
-  print(f"\t- type: {type(html)}")
-  print(f"\t- content: {html}")
-  print(f"\t- utf8 content: {html.decode('utf-8')}")
+if __name__ == "__main__":
+  URI = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+  with urllib.request.urlopen(URI) as response:
+    html = response.read()
+    print("Body response:")
+    print(f"\t- type: {type(html)}")
+    print(f"\t- content: {html}")
+    print(f"\t- utf8 content: {html.decode('utf-8')}")
