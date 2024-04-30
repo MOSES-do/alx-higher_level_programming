9c76c25a0d827bde15075bcead736639fb3d49c1
Esumei Moses <archer.me24@gmail.com>
2024-04-30 04:44:29 -0700

    using .get to retrieve key value from response header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """Python script to request response  and display X-Request-Id value
-from header object and key values using ".get('key')"""
+from header object and key values using ".get('key')
+"""
 
 
-import urllib
+import urllib.request
 import sys
 
 
 if __name__ == '__main__':
-    """script to fetch value from response header"""
     with urllib.request.urlopen(sys.argv[1]) as response:
         html_id = response.headers.get('X-Request-Id')
         print(html_id)
