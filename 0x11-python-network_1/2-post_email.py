a6e43a88d96100d5b2bbdd02bdd0ac0670e4a657
Esumei Moses <archer.me24@gmail.com>
2024-04-30 05:22:30 -0700

    send data to server using urllib

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""send an email as parameter to a url"""
+
+import urllib.request
+import urllib.parse
+import sys
+
+url = sys.argv[1]
+email = sys.argv[2]
+if __name__ == "__main__":
+    data = urllib.parse.urlencode({'email': sys.argv[2]})
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data, method='POST')
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
