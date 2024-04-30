13e4cb598bc886f4e292c10bc251e7878ce8e18c
Esumei Moses <archer.me24@gmail.com>
2024-04-30 08:58:50 -0700

    fetch srever's response using python's HTTP library called requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     r = requests.get(url).text
     print("Body response:")
-    print(f"\t - type: {type(r)}")
-    print(f"\t - content: {r}")
+    print(f"    - type: {type(r)}")
+    print(f"    - content: {r}")
