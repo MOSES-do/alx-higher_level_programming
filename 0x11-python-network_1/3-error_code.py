e3dfe4ea0689e2a9cb8b69e69315975e8918bc18
Esumei Moses <archer.me24@gmail.com>
2024-04-30 06:48:01 -0700

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """display body of a client to server response"""
 
-from urllib.request import Request, urlopen
-from urllib.error import HTTPError
+import urllib.request
+import urllib.error
 import sys
 
 
 if __name__ == '__main__':
-    req = Request(sys.argv[1])
+    req = urllib.request.Request(sys.argv[1])
     try:
-        response = urlopen(req)
-    except HTTPError as e:
+        response = urllib.request.urlopen(req)
+    except urllib.error.HTTPError as e:
         print(f"Error code: {e.code}")
     else:
         print(response.read().decode('utf-8'))
