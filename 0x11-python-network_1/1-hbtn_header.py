27bb1227d69c8dfa0500f6db043783e632cc73df
Esumei Moses <archer.me24@gmail.com>
2024-04-30 04:46:35 -0700

    using .get to retrieve key value from response header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,6 +7,7 @@ import sys
 
 
 if __name__ == '__main__':
+    """from header response object key:value pairs using ".get('key')"""
     with urllib.request.urlopen(sys.argv[1]) as response:
         html_id = response.headers.get('X-Request-Id')
         print(html_id)
