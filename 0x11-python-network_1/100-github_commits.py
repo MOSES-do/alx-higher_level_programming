fa5b418da393b4379a66d0fddadb2e85d312b728
Esumei Moses <archer.me24@gmail.com>
2024-04-30 22:12:04 -0700

    interview prep: list 10 commits using Github's API https://developer.github.com/v3/repos/commits/

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,9 +9,21 @@ import sys
 
 if __name__ == "__main__":
     """and uses the github API to display my id"""
-    url = f"https://api.github.com/repos/{sys.argv[1]}/{sys.argv[2]}/commits"
-    req = requests.get(url)
-    if req.status_code == 200:
-        last_10_commits = req.json()
-        for commit in last_10_commits[:10]:
-            print(f"{commit['sha']}: {commit['commit']['author']['name']}")
+    if len(sys.argv) > 2:
+        url = f"https://api.github.com/repos/{sys.argv[1]}\
+        /{sys.argv[2]}/commits"
+        req = requests.get(url)
+        if req.status_code == 200:
+            last_10_commits = req.json()
+            for commit in last_10_commits[:10]:
+                print(f"{commit['sha']}: {commit['commit']['author']['name']}")
+    else:
+        params = sys.argv[1].split("/")
+        u_name = params[0]
+        repo = params[1]
+        url = f"https://api.github.com/repos/{u_name}/{repo}/commits"
+        req = requests.get(url)
+        if req.status_code == 200:
+            last_10_commits = req.json()
+            for commit in last_10_commits[:10]:
+                print(f"{commit['sha']}: {commit['commit']['author']['name']}")
