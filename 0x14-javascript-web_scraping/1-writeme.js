2ecba733d33947de8545c628bed504e3652cf51f
Esumei Moses <archer.me24@gmail.com>
2024-05-23 05:01:32 -0700

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// A script that reads and prints the content of a file
+// A script that writes and prints the content to a file
 
 const fs = require('fs');
 const file = process.argv[2];
@@ -8,6 +8,5 @@ const text = process.argv[3];
 fs.writeFile(file, text, 'utf8', (err) => {
   if (err) {
     console.error('Error writing to file:', err);
-    return;
   }
 });
