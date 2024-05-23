1dc7db8513073d6d62a48244d42a0eea6ba49653
Esumei Moses <archer.me24@gmail.com>
2024-05-23 09:07:11 -0700

    5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -7,15 +7,12 @@ const url = process.argv[2];
 const fileName = process.argv[3];
 
 const scrapWeb = () => {
-  request(url, { json: true }, (error, response, body) => {
+  request(url, (error, response, body) => {
     if (error) console.log(error);
 
     fs.writeFile(fileName, body, 'utf-8', (err) => {
-      if (err) {
-        console.error(err);
-      }
+      if (err) console.error(err);
     });
   });
 };
-
 scrapWeb();
